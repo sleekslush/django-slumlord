e784c3e6daced9b126585b90e4da4f342a9239c7
Craig Slusher <cslush@gmail.com>
2011-12-08 12:51:23 -0800

    Removes domain-level methods.
    The methods should not have been here to begin with.
    
    Fixes #1

--- saas/models.py
+++ saas/models.py
@@ -7,19 +7,12 @@ class Tenant(models.Model):
     Represents a tenant or client in a multi-tenant application.
     """
     owner = models.ForeignKey(User)
+    created_at = models.DateTimeField(auto_now_add=True)
+    activated_at = models.DateTimeField(null=True)
     subdomain = models.CharField(max_length=100, unique=True)
     custom_domain = models.URLField(unique=True, blank=True)
 
     objects = TenantManager()
-
-    def get_app_schema(self):
-        return 'app_{}'.format(self.id)
-
-    def get_users_schema(self):
-        return '{}_users'.format(self.get_app_schema())
-
-    def get_full_schema(self):
-        return '{}, {}'.format(self.get_app_schema(), self.get_users_schema())
 
     def __unicode__(self):
         return self.custom_domain or self.subdomain
